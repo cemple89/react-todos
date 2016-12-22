ef3ca8dd80ef80a74811e724d1a68e2eee2c42f3
Charlie Emple <charlie.emple@gmail.com>
2016-12-22 17:14:00 -0500

    importing todos-list to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import TodosList from './todos-list';
+
+const todos = [
+{
+	task: 'make React tutorial',
+	isCompleted: false
+},
+{
+	task: 'eat dinner',
+	isCompleted: true
+}
+];
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>React ToDos Apps</h1>
+				<TodosList />
 			</div>
 
 		);
