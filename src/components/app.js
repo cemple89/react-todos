591b2a3cbcc74c36eaebad6474519660be95bdc7
Charlie Emple <charlie.emple@gmail.com>
2016-12-27 12:25:38 -0500

    adding task functionality 33m

--- src/components/app.js
+++ src/components/app.js
@@ -27,11 +27,21 @@ export default class App extends React.Component {
 		return (
 			<div>
 				<h1>React ToDos Apps</h1>
-				<CreateTodo />
-				<TodosList todos={this.state.todos} />
+				<CreateTodo createTask={this.createTask.bind(this)} />
+				<TodosList 
+					todos={this.state.todos} 
+				/>
 			</div>
 
 		);
 
+	}
+
+	createTask(task) {
+		this.state.todos.push({
+			task,
+			isCompleted: false 
+		});
+		this.setState({todos: this.state.todos });
 	}
 }
--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -4,12 +4,17 @@ import React from 'react';
 export default class TodosList extends React.Component {
 	render() {
 		return (
-			<form>
-				<input type="text" placeholder="What do I need to do?" />
+			<form onSubmit={this.handleCreate.bind(this)}>
+				<input type="text" placeholder="What do I need to do?" ref="createInput" />
 				<button>Create</button>
 			</form>
 
 		);
 
+	}
+	handleCreate(event) {
+		event.preventDefault();
+
+		this.props.createTask(this.refs.createInput.value);
 	}
 }
