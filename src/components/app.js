2b6a1686282b1530fb42a03d12745a8ada45b9eb
Charlie Emple <charlie.emple@gmail.com>
2016-12-27 10:46:21 -0500

    create todo entry form

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CreateTodo from './create-todo';
 import TodosList from './todos-list';
 
 const todos = [
@@ -26,6 +27,7 @@ export default class App extends React.Component {
 		return (
 			<div>
 				<h1>React ToDos Apps</h1>
+				<CreateTodo />
 				<TodosList todos={this.state.todos} />
 			</div>
 
